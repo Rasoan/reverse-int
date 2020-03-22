7760851f996dc41172cbb6920283f2fb0ae696fd
Araik Rasoyan <araikrasoian@gmail.com>
2020-03-22 11:47:14 +0300

    init: start reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function reverse (n) {
-  
+  let array = n.toString().split('');
+  let strings = '';
+  console.log(array);
+  array.forEach( function f(element, index, arr)  {
+
+
+      strings += (arr[arr.length -1 -index] != '-') && (arr[arr.length -1 -index] != '+') ? arr[arr.length -1 -index]: '';
+  });
+ 
+  //console.log(strings);
+
+ return +strings;
 }
+
+
